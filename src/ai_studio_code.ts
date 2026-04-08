28ce75c43d6e3eb5ed608156455c166e9bea998f
nuellprosper <nuellprosper25@gmail.com>
2026-04-08 09:17:31 +0100

    Delete src/ai_studio_code.ts

--- src/ai_studio_code.ts
+++
@@ -1,6 +0,0 @@
-import { 
-  auth, db, googleProvider, signInWithPopup, signOut, onAuthStateChanged,
-  doc, getDoc, setDoc, updateDoc, deleteDoc, collection, query, where, onSnapshot, getDocs, addDoc, serverTimestamp,
-  createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, // <-- Add this
-  FirestoreOperation, handleFirestoreError
-} from './firebase';
