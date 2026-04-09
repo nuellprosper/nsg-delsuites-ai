e033feee6c110bd283a08dc7b1e16bc56915857e
nuellprosper <nuellprosper25@gmail.com>
2026-04-09 08:45:23 +0100

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// Minimal service worker for PWA installability
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass through all requests
+  event.respondWith(fetch(event.request));
+});
